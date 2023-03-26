86826d664f6027352bb5ec54b135dba41ceb9931
Parth Shrotri <shrotri.parth@gmail.com>
2023-03-26 14:25:31 -0500

    bug fixes

--- simulation/2_body_main.py
+++ simulation/2_body_main.py
@@ -11,10 +11,10 @@ sys.path.insert(0, os.path.abspath(
 from dynamics.body import Body
 from properties import prop
 
-day_in_sec = 24*60*60
-days = 365
+seconds_per_day = 24*60*60
+days = 5*365
 earth_pos = np.array([-2.600745330322259E+10,  1.326237828764555E+11,  5.752511716959091E+10])
-earth_vel = np.array([-2.983800155786667E+01, -4.724379075830139E+00, -2.047849455589723E+00,])*day_in_sec
+earth_vel = np.array([-2.983800155786667E+04, -4.724379075830139E+03, -2.047849455589723E+03,])*seconds_per_day
 
 # Define system bodies
 sun = Body(prop.mass_sun, np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), False, 'sun')
@@ -26,7 +26,7 @@ for body in system_bodies:
 
 sim = Simulator()
 t_start = time.time()
-sim.rk4_ivp(system_bodies, 0, days, 60/(day_in_sec))
+sim.rk4_ivp(system_bodies, 0, days, 60/(seconds_per_day))
 t_end = time.time() - t_start
 print(f"Time: {t_end:.2f}")
 
