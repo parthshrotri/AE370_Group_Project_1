3d0f0e96f5975b4ca9bff73487ebf6baf71c4daa
evandy2 <evandy2@illinois.edu>
2023-03-29 00:30:25 -0500

    jank mp stuff

--- simulation/local_trunc.py
+++ simulation/local_trunc.py
@@ -3,28 +3,29 @@ from two_body_main import two_body_problem
 from three_body_main import three_body_problem
 import os
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 baseline_delta_t = 30
 delta_t_vals = np.logspace(np.log10(30), np.log10(3600), 20)
 
 def two_body_local_trunc(delta_t, days, baseline_last):
     two_body_problem(days, delta_t, False)
-    appx_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/two_body_test/', 'earth_trajectory.npy'))
+    appx_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/two_body_test_{delta_t}/', 'earth_trajectory.npy'))
     appx_last = appx_traj[-1]
     err = np.linalg.norm(appx_last - baseline_last)/np.linalg.norm(baseline_last)
     return err
 
 def two_body_baseline(baseline_delta_t, days = 365):
     two_body_problem(days, baseline_delta_t, False)
-    baseline_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/two_body_test/', 'earth_trajectory.npy'))
+    baseline_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/two_body_test_{baseline_delta_t}/', 'earth_trajectory.npy'))
     baseline_last = baseline_traj[-1]
     return baseline_last
 
 def three_body_local_trunc(delta_t, days, baseline_last):
     three_body_problem(days, delta_t, False)
-    earth_appx_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/three_body_test/', 'earth_trajectory.npy'))
+    earth_appx_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/three_body_test_{delta_t}/', 'earth_trajectory.npy'))
     earth_appx_last = earth_appx_traj[-1]
-    moon_appx_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/three_body_test/', 'moon_trajectory.npy'))
+    moon_appx_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/three_body_test_{delta_t}/', 'moon_trajectory.npy'))
     moon_appx_last = moon_appx_traj[-1]
     earth_err = np.linalg.norm(earth_appx_last - baseline_last[0])/np.linalg.norm(baseline_last[0])
     moon_err = np.linalg.norm(moon_appx_last - baseline_last[1])/np.linalg.norm(baseline_last[1])
@@ -32,9 +33,9 @@ def three_body_local_trunc(delta_t, days, baseline_last):
 
 def three_body_baseline(baseline_delta_t, days = 365):
     three_body_problem(days, baseline_delta_t, False)
-    earth_baseline_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/three_body_test/', 'earth_trajectory.npy'))
+    earth_baseline_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/three_body_test_{baseline_delta_t}/', 'earth_trajectory.npy'))
     earth_baseline_last = earth_baseline_traj[-1]
-    moon_baseline_traj = np.load(os.path.join(os.path.dirname(__file__), '../output/three_body_test/', 'moon_trajectory.npy'))
+    moon_baseline_traj = np.load(os.path.join(os.path.dirname(__file__), f'../output/three_body_test_{baseline_delta_t}/', 'moon_trajectory.npy'))
     moon_baseline_last = moon_baseline_traj[-1]
     baseline_last = [earth_baseline_last, moon_baseline_last]
     return baseline_last
@@ -45,15 +46,28 @@ earth_three_body_errors, moon_three_body_errors = [], []
 ctr = 1
 two_body_baseline_last = two_body_baseline(baseline_delta_t, days)
 three_body_baseline_last = three_body_baseline(baseline_delta_t, days)
-for delta_t in delta_t_vals:
+
+# for delta_t in delta_t_vals:
+    
+#     two_body_errors.append(two_body_local_trunc(delta_t, days, two_body_baseline_last))
+#     three_error = three_body_local_trunc(delta_t, days, three_body_baseline_last)
+#     earth_three_body_errors.append(three_error[0])
+#     moon_three_body_errors.append(three_error[1])
+#     print(ctr)
+#     ctr += 1
+def multiprocess(delta_t):
     two_body_errors.append(two_body_local_trunc(delta_t, days, two_body_baseline_last))
     three_error = three_body_local_trunc(delta_t, days, three_body_baseline_last)
     earth_three_body_errors.append(three_error[0])
     moon_three_body_errors.append(three_error[1])
-    print(ctr)
-    ctr += 1
+    # print(ctr)
+    # ctr += 1
 
+with mp.Pool() as pool:
+    pool.map(multiprocess, delta_t_vals)
+    
 plt.figure()
+plt.plot(delta_t_vals, delta_t_vals**4, '-', label = '$\Delta t^4$')
 plt.plot(delta_t_vals, two_body_errors, '-', label = 'Earth error')
 plt.xscale('log')
 plt.yscale('log')
@@ -63,6 +77,7 @@ plt.title('Local truncation error vs $\Delta$ t for 2-body problem')
 plt.legend()
 
 plt.figure()
+plt.plot(delta_t_vals, delta_t_vals**4, '-', label = '$\Delta t^4$')
 plt.plot(delta_t_vals, earth_three_body_errors, '-', label = 'Earth error')
 plt.plot(delta_t_vals, moon_three_body_errors, '-', label = 'Moon error')
 plt.xscale('log')
--- simulation/two_body_main.py
+++ simulation/two_body_main.py
@@ -10,7 +10,7 @@ from plotter import plot
 from dynamics.body import Body
 from properties import prop
 
-def two_body_problem(days = 365, dt = 60, plt = False):
+def two_body_problem(days = 365, dt = 60, plt = False, save_folder = None):
 
     seconds_per_day = 24*60*60
     earth_pos = prop.earth_initial_position
@@ -26,7 +26,7 @@ def two_body_problem(days = 365, dt = 60, plt = False):
 
     sim = Simulator()
     t_start = time.time()
-    sim.rk4_ivp(system_bodies, 0, days, dt/(seconds_per_day), '../output/two_body_test/')
+    sim.rk4_ivp(system_bodies, 0, days, dt/(seconds_per_day), f'../output/two_body_test_{dt}/')
     t_end = time.time() - t_start
     print(f'Run at dt = {dt:.2f} completed in {t_end:.2f}sec')
 
